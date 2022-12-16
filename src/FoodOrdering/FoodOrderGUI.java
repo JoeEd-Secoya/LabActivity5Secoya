c834ea0b12fea4c93c6327b5a0d71d368a9be0f9
Matrix <joeed.secoya@cit.edu>
2022-12-17 04:13:28 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
@@ -22,5 +24,49 @@ public class FoodOrderGUI extends JFrame{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(700, 500);
         frame.setVisible(true);
+    }
+
+    public FoodOrderGUI(){
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                order();
+            }
+        });
+    }
+    public void order(){
+            double total = 0;
+            int discount = 0;
+
+            if (cPizza.isSelected()) {
+                total += 100;
+            }
+            if (cBurger.isSelected()) {
+                total += 80;
+            }
+            if (cFries.isSelected()) {
+                total += 65;
+            }
+            if (cSoftDrinks.isSelected()) {
+                total += 55;
+            }
+            if (cTea.isSelected()) {
+                total += 50;
+            }
+            if (cSundae.isSelected()) {
+                total += 40;
+            }
+
+            if (rb5.isSelected()) {
+                discount += 5;
+            }
+            if (rb10.isSelected()) {
+                discount += 10;
+            }
+            if (rb15.isSelected()) {
+                discount += 15;
+            }
+            total -= total * discount / 100;
+            JOptionPane.showMessageDialog(null, String.format("Total price is Php %.2f", total));
     }
 }
